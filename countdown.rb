d4797715807cd946f46cfe038185ac1b1287bbe1
James Pier <jpier2012@gmail.com>
2019-04-01 14:41:15 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,14 @@ def countdown number
   end
   "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep number
+  counter = number
+  while counter > 0
+    puts "#{counter} SECOND(S)!"
+    counter -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+
